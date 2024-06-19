686186a2d3d11e7e5ea08e7483ec22eb77215fa6
Zak <zakariah8@outlook.com>
2024-06-18 21:30:18 -0400

    database initialization

--- musicapp/api/views.py
+++ musicapp/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+#an endpoint is the thing after slash\
+
+def main(request):
+     return HttpResponse("Hello")
